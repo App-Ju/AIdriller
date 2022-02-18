aed4edebdea94a2c400365a044bddf047ef031da
AppJu <appjuice@mail.ru>
2022-02-18 17:42:27 +0700

    AddressSearch component is ready

--- src/components/AddressSearchPage.vue
+++ src/components/AddressSearchPage.vue
@@ -1,46 +1,154 @@
 <template>
-  <div>
-    <hr>
-    поле для ввода адреса, кнопка Search
-    <v-btn @click="push">
+  <form class="search__form container">
+    <v-text-field
+        v-model="query"
+        label="Введите адрес"
+        @keydown.enter.prevent="push"
+        class="search__input"
+    ></v-text-field>
+    <v-btn
+        class="search__btn"
+        @click="push"
+    >
       Поиск
     </v-btn>
-    <hr>
-    После ввода адреса, нажимаем кнопку, происходит запрос данных с api сервиса
-    Dadata (https://dadata.ru/api/suggest/).
-    <hr>
-    После получения данных отображаем результат в таблице: Zip(Индекс), City(Город), Street(Улица),
-    House(Дом), Flat(Квартира).
-  </div>
-  <br>
-  <div>
-    <div>Zip(Индекс)</div>
-    <div>{{getResult[0].postal_code}}</div>
-    <div>City(Город)</div>
-    <div>{{getResult[0].region}}</div>
-    <div>Street(Улица)</div>
-    <div>{{getResult[0].street}}</div>
-    <div>House(Дом)</div>
-    <div>{{getResult[0].house}}</div>
-    <div>Flat(Квартира)</div>
-    <div>{{getResult[0].flat}}</div>
-  </div>
+  </form>
+  <v-card-text
+      v-if="getResult.length < 1"
+      class="search__previously container"
+  >
+    Введите адрес и нажмите кнопку.
+  </v-card-text>
+
+  <v-container
+      v-else
+      class="search__result container"
+  >
+    <v-row>
+      <v-col
+          cols="3"
+          class="result__col ml-auto"
+      >
+        Zip(Индекс)
+      </v-col>
+      <v-col
+          cols="3"
+          class="result__col mr-auto"
+      >
+        {{ getResult[0].postal_code || 'Нет совпадений' }}
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+          cols="3"
+          class="result__col ml-auto"
+      >
+        City(Город)
+      </v-col>
+      <v-col
+          cols="3"
+          class="result__col mr-auto"
+      >
+        {{ getResult[0].city || getResult[0].region || 'Нет совпадений' }}
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+          cols="3"
+          class="result__col ml-auto"
+      >
+        Street(Улица)
+      </v-col>
+      <v-col
+          cols="3"
+          class="result__col mr-auto"
+      >
+        {{ getResult[0].street || 'Нет совпадений' }}
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+          cols="3"
+          class="result__col ml-auto"
+      >
+        House(Дом)
+      </v-col>
+      <v-col
+          cols="3"
+          class="result__col mr-auto"
+      >
+        {{ getResult[0].house || 'Нет совпадений' }}
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+          cols="3"
+          class="result__col ml-auto"
+      >
+        Flat(Квартира)
+      </v-col>
+      <v-col
+          cols="3"
+          class="result__col mr-auto"
+      >
+        {{ getResult[0].flat || 'Нет совпадений' }}
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
+import {mapGetters, mapActions} from 'vuex'
 
 export default {
   name: "AddressSearchPage",
+  data() {
+    return {
+      query: ''
+    }
+  },
   computed: mapGetters(['getResult']),
   methods: {
+    ...mapActions(['requestAddress']),
     push() {
-      this.$store.dispatch('requestAddress')
+      console.log('q', this.query)
+      if (this.query.trim()) {
+        this.requestAddress(this.query)
+      }
+      this.query = ''
     }
   }
 }
 </script>
 
 <style scoped>
+.search__form {
+  margin: 10% auto 5% auto;
+}
 
+.search__btn {
+  display: block;
+  margin: 0 auto;
+}
+
+.search__previously {
+  text-align: center;
+  font-size: 20px;
+  padding: 20px 0;
+  background: #9cd9dc;
+  width: 50%;
+}
+
+.search__result {
+  font-size: 18px;
+}
+
+.result__col {
+  height: 40px;
+  background: #9cd9dc;
+  border-top: 1px solid;
+  border-bottom: 1px solid;
+  padding: 10px 0 0 10px;
+  margin-bottom: 10px;
+}
 </style>
--- src/store/modules/dadata.js
+++ src/store/modules/dadata.js
@@ -1,13 +1,12 @@
 export default {
     actions: {
-        async requestAddress(context) {
+        async requestAddress(context, query) {
             // необходимо посетить https://cors-anywhere.herokuapp.com/corsdemo и
             // активировать демо режим для работы прокси сервера
-            // const proxyURL = 'https://cors-anywhere.herokuapp.com/'
             const url = "https://cors-anywhere.herokuapp.com/https://cleaner.dadata.ru/api/v1/clean/address";
             const token = "2dc9501086d6e922a76115e4ec68c517acbdc38c";
             const secret = "c6e10a0bb0855f861d3067059308cba1dd5c4558";
-            const query = "мск сухонска 11/-89";
+            // const query = "мск сухонска 11/-89";
 
             const options = {
                 method: "POST",
