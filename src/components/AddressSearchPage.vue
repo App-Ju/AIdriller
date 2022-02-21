3191eecc458e24ee22d094d9d1ad97353ce11454
AppJu <appjuice@mail.ru>
2022-02-21 20:17:16 +0700

    cover and description for GuideAboutPage

--- src/components/AddressSearchPage.vue
+++ src/components/AddressSearchPage.vue
@@ -1,154 +1,164 @@
 <template>
-  <form class="search__form container">
-    <v-text-field
-        v-model="query"
-        label="Введите адрес"
-        @keydown.enter.prevent="push"
-        class="search__input"
-    ></v-text-field>
-    <v-btn
-        class="search__btn"
-        @click="push"
-    >
-      Поиск
-    </v-btn>
-  </form>
-  <v-card-text
-      v-if="getResult.length < 1"
-      class="search__previously container"
-  >
-    Введите адрес и нажмите кнопку.
-  </v-card-text>
+	<form class="search__form container">
+		<v-text-field
+			v-model="query"
+			label="Введите адрес"
+			@keydown.enter.prevent="push"
+			class="search__input"
+		></v-text-field>
+		<div class="search__btn">
+			<v-btn @click="push">
+				Поиск
+			</v-btn>
+		</div>
+	</form>
+	<div
+		class="search__previously"
+		v-if="getResult.length < 1"
+	>
+		Введите адрес и нажмите кнопку.
+	</div>
 
-  <v-container
-      v-else
-      class="search__result container"
-  >
-    <v-row>
-      <v-col
-          cols="3"
-          class="result__col ml-auto"
-      >
-        Zip(Индекс)
-      </v-col>
-      <v-col
-          cols="3"
-          class="result__col mr-auto"
-      >
-        {{ getResult[0].postal_code || 'Нет совпадений' }}
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col
-          cols="3"
-          class="result__col ml-auto"
-      >
-        City(Город)
-      </v-col>
-      <v-col
-          cols="3"
-          class="result__col mr-auto"
-      >
-        {{ getResult[0].city || getResult[0].region || 'Нет совпадений' }}
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col
-          cols="3"
-          class="result__col ml-auto"
-      >
-        Street(Улица)
-      </v-col>
-      <v-col
-          cols="3"
-          class="result__col mr-auto"
-      >
-        {{ getResult[0].street || 'Нет совпадений' }}
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col
-          cols="3"
-          class="result__col ml-auto"
-      >
-        House(Дом)
-      </v-col>
-      <v-col
-          cols="3"
-          class="result__col mr-auto"
-      >
-        {{ getResult[0].house || 'Нет совпадений' }}
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col
-          cols="3"
-          class="result__col ml-auto"
-      >
-        Flat(Квартира)
-      </v-col>
-      <v-col
-          cols="3"
-          class="result__col mr-auto"
-      >
-        {{ getResult[0].flat || 'Нет совпадений' }}
-      </v-col>
-    </v-row>
-  </v-container>
+	<v-container
+		v-else
+		class="search__result container"
+	>
+		<v-row>
+			<v-col
+				cols="3"
+				class="result__col ml-auto"
+			>
+				Zip(Индекс)
+			</v-col>
+			<v-col
+				cols="3"
+				class="result__col mr-auto"
+			>
+				{{ getResult[0].postal_code || 'Нет совпадений' }}
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col
+				cols="3"
+				class="result__col ml-auto"
+			>
+				City(Город)
+			</v-col>
+			<v-col
+				cols="3"
+				class="result__col mr-auto"
+			>
+				{{ getResult[0].city || getResult[0].region || 'Нет совпадений' }}
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col
+				cols="3"
+				class="result__col ml-auto"
+			>
+				Street(Улица)
+			</v-col>
+			<v-col
+				cols="3"
+				class="result__col mr-auto"
+			>
+				{{ getResult[0].street || 'Нет совпадений' }}
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col
+				cols="3"
+				class="result__col ml-auto"
+			>
+				House(Дом)
+			</v-col>
+			<v-col
+				cols="3"
+				class="result__col mr-auto"
+			>
+				{{ getResult[0].house || 'Нет совпадений' }}
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col
+				cols="3"
+				class="result__col ml-auto"
+			>
+				Flat(Квартира)
+			</v-col>
+			<v-col
+				cols="3"
+				class="result__col mr-auto"
+			>
+				{{ getResult[0].flat || 'Нет совпадений' }}
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
 import {mapGetters, mapActions} from 'vuex'
 
 export default {
-  name: "AddressSearchPage",
-  data() {
-    return {
-      query: ''
-    }
-  },
-  computed: mapGetters(['getResult']),
-  methods: {
-    ...mapActions(['requestAddress']),
-    push() {
-      console.log('q', this.query)
-      if (this.query.trim()) {
-        this.requestAddress(this.query)
-      }
-      this.query = ''
-    }
-  }
+	name: "AddressSearchPage",
+	data() {
+		return {
+			query: ''
+		}
+	},
+	computed: mapGetters(['getResult']),
+	methods: {
+		...mapActions(['requestAddress']),
+		push() {
+			console.log('q', this.query)
+			if (this.query.trim()) {
+				this.requestAddress(this.query)
+			}
+			this.query = ''
+		}
+	}
 }
 </script>
 
 <style scoped>
+
 .search__form {
-  margin: 10% auto 5% auto;
+	margin: 10% auto 5% auto;
+}
+
+.search__input {
+	position: relative;
+	width: 80%;
+	margin: 0 auto;
 }
 
 .search__btn {
-  display: block;
-  margin: 0 auto;
+	position: relative;
+	display: block;
+	left: 45%;
 }
 
 .search__previously {
-  text-align: center;
-  font-size: 20px;
-  padding: 20px 0;
-  background: #9cd9dc;
-  width: 50%;
+	position: relative;
+	text-align: center;
+	font-size: 20px;
+	padding: 20px 0;
+	background: #9cd9dc;
+	width: 50%;
+	left: 25%;
 }
 
 .search__result {
-  font-size: 18px;
+	font-size: 18px;
 }
 
 .result__col {
-  height: 40px;
-  background: #9cd9dc;
-  border-top: 1px solid;
-  border-bottom: 1px solid;
-  padding: 10px 0 0 10px;
-  margin-bottom: 10px;
+	height: 40px;
+	background: #9cd9dc;
+	border-top: 1px solid;
+	border-bottom: 1px solid;
+	padding: 10px 0 0 10px;
+	margin-bottom: 10px;
 }
+
 </style>
--- src/components/GuideAboutPage.vue
+++ src/components/GuideAboutPage.vue
@@ -1,101 +1,107 @@
 <template>
-  <v-card class="about__guide">
-    <AddressSearchPage/>
-    <div class="about__cover">
-      <div class="cover__input">
-        <div class="cover__arrow cover__up"/>
-        <span>Поле для ввода адреса в свободной форме</span>
-      </div>
-      <div class="cover__btn">
-        <div class="cover__arrow cover__left"/>
-        <span>При нажатии кнопки запускается поиск <br>адреса соответствующего введеному
-        значению</span>
-      </div>
-      <div class="cover__result">
-        <div class="cover__arrow cover__down"/>
-        <span>Здесь отображается результат поиска</span>
-      </div>
-    </div>
-  </v-card>
-  <div class="container mt-6">
-    Отобразить вид С пояснениями к каждому контролу по его функциональности.
-    Вывести текстовое описание того, как работает поиск в целом.
-  </div>
+	<v-card class="about__guide">
+		<AddressSearchPage/>
+		<div class="about__cover"/>
+	</v-card>
+	<div class="container mt-6">
+		Отобразить вид С пояснениями к каждому контролу по его функциональности.
+		Вывести текстовое описание того, как работает поиск в целом.
+	</div>
 </template>
 
 <script>
 import AddressSearchPage from "@/components/AddressSearchPage";
 
 export default {
-  name: "GuideAboutPage",
-  components: {AddressSearchPage}
+	name: "GuideAboutPage",
+	components: {AddressSearchPage}
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
 span {
-  margin-left: 30px;
+	margin-left: 30px;
 }
 
 .about__guide {
-  transform: scale(0.65);
-  padding-bottom: 3%;
+	transform: scale(0.65);
+	padding-bottom: 3%;
 }
 
 .about__cover {
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  font-size: 24px;
+	height: 100%;
+	width: 100%;
+	position: absolute;
+	top: 0;
+	font-size: 24px;
 }
 
-.cover__input {
-  position: absolute;
-  top: 47%;
-  left: 10.5%;
+:deep(.search__form) {
+
+	.search__input:before {
+		content: '';
+		position: absolute;
+		width: 20px;
+		height: 20px;
+		box-sizing: border-box;
+		border-left: 6px solid #337AB7;
+		border-bottom: 6px solid #337AB7;
+		top: 67px;
+		left: 10px;
+		transform: rotate(135deg);
+	}
+
+	.search__input:after {
+		font-size: 22px;
+		content: 'Поле для ввода адреса в свободной форме';
+		position: absolute;
+		top: 65px;
+		left: 40px;
+	}
+
+	.search__btn:before {
+		content: '';
+		position: absolute;
+		width: 20px;
+		height: 20px;
+		box-sizing: border-box;
+		border-left: 6px solid #337AB7;
+		border-bottom: 6px solid #337AB7;
+		top: 8px;
+		left: 100px;
+		transform: rotate(45deg);
+	}
+
+	.search__btn:after {
+		font-size: 22px;
+		content: 'При нажатии кнопки запускается поиск адреса соответствующего введеному значению';
+		position: absolute;
+		left: 120px;
+		width: 45%;
+	}
+
 }
 
-.cover__btn {
-  position: absolute;
-  top: 55%;
-  left: 54%;
+:deep(.search__previously:before) {
+	content: '';
+	position: absolute;
+	width: 20px;
+	height: 20px;
+	box-sizing: border-box;
+	border-left: 6px solid #337AB7;
+	border-bottom: 6px solid #337AB7;
+	top: -35px;
+	left: 10px;
+	transform: rotate(-45deg);
 }
 
-.cover__result {
-  position: absolute;
-  top: 70%;
-  left: 35%;
+:deep(.search__previously:after) {
+	font-size: 20px;
+	content: 'Здесь отображается результат поиска';
+	position: absolute;
+	top: -30px;
+	left: 40px;
 }
 
-.cover__arrow {
-  position: absolute;
-  top: 5px;
-  width: 20px;
-  height: 20px;
-}
-
-.cover__arrow:after {
-  content: '';
-  position: absolute;
-  box-sizing: border-box;
-  width: 100%;
-  height: 100%;
-  border-left: 6px solid #337AB7;
-  border-bottom: 6px solid #337AB7;
-}
-
-.cover__up {
-  transform: rotate(135deg);
-}
-
-.cover__left {
-  transform: rotate(45deg);
-  top: 0;
-}
-
-.cover__down {
-  transform: rotate(-45deg);
-  top: -5px;
-}
 </style>
